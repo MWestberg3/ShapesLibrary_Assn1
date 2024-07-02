c5fa90cd9e93526ef48f264e99baf2a577769869
Michael Westberg <mwestberg@bamboohr.com>
2024-07-01 18:25:07 -0600

    adding point test

--- src/test/kotlin/PointTest.kt
+++ src/test/kotlin/PointTest.kt
@@ -1,2 +1,26 @@
+import kotlin.test.assertEquals
+import kotlin.test.Test
+import org.example.Point
+
 class PointTest {
+    @Test
+    fun testPoint() {
+        val point = Point(1.0, 2.0)
+        assertEquals(1.0, point.getX())
+        assertEquals(2.0, point.getY())
+    }
+    @Test
+    fun testClonePoint() {
+        val point = Point(1.0, 2.0)
+        val clone = point.clonePoint()
+        assertEquals(1.0, clone.getX())
+        assertEquals(2.0, clone.getY())
+    }
+    @Test
+    fun testMovePoint() {
+        val point = Point(1.0, 2.0)
+        point.movePoint(3.0, 4.0)
+        assertEquals(4.0, point.getX())
+        assertEquals(6.0, point.getY())
+    }
 }
